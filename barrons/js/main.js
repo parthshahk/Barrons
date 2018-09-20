d79b0cb50c3f867636d1370ba25122bfd2971882
Parth Shah <parthshahk@gmail.com>
2018-09-20 22:58:36 +0530

    Finished

---
+++ barrons/js/main.js
@@ -0,0 +1,74 @@
+var app = new Vue({
+    el: '#app',
+
+    data: {
+        view: 'start',
+        word: '',
+        defs: [],
+        activeDef: '',
+        nextWord:'',
+        nextDefs:[]
+    },
+
+    methods: {
+
+        cycleDef: function(){
+
+            var length  = this.defs.length;
+            
+            if(this.activeDef == length-1)
+                this.activeDef = 0;
+            else
+                this.activeDef++;
+            
+        },
+
+        fetchWord: function(){
+        
+            var cacheControl = Math.random();
+
+            var self = this;
+
+            this.word = this.nextWord;
+            this.defs = this.nextDefs;
+            this.activeDef = this.defs.length-1;
+
+            axios.get('./api/words.php?action=getRandom&nocache='+cacheControl)
+                .then(function(response){
+
+                    self.nextWord = response.data.word;
+                    self.nextDefs = response.data.definition;
+                    self.nextDefs.push("");
+                })
+        }
+    },
+
+    mounted: function(){
+
+        var cacheControl = Math.random();
+
+        var self = this;
+        axios.get('./api/words.php?action=getRandom&nocache='+cacheControl)
+            .then(function(response){
+
+                self.nextWord = response.data.word;
+                self.nextDefs = response.data.definition;
+                self.nextDefs.push("");
+                self.activeDef = self.nextDefs.length-1;
+                self.fetchWord();
+            })
+    }
+});
+
+var appWrapper = document.getElementById("appBackground");
+var hammertime = new Hammer(appWrapper);
+
+hammertime.on('swipeleft', function() {
+    app.fetchWord();
+});
+
+hammertime.on('swipeup', function() {
+    app.cycleDef();
+});
+
+hammertime.get('swipe').set({ velocity: 0.1, direction: Hammer.DIRECTION_ALL });
