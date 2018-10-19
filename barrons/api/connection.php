9e5faa2b6d21f08ee6c9b4ed6094ecefa227a57a
Parth Shah <parthshahk@gmail.com>
2018-10-19 10:37:55 +0530

    .

--- barrons/api/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","parth","parth","vocab");
-    // $con=mysqli_connect("mysql.hostinger.in","u696737897_parth","`7w4]8io`aR8xu1AB1","u696737897_datab");
-
-?>
